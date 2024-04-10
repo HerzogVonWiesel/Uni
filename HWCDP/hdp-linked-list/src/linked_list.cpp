180f2e23b66ef991d63c8349b2a7d12e197df025
Marvin Jerome Stephan <hello@jeromestephan.de>
2024-04-10 20:31:05 +0200

    hwcdp

--- HWCDP/hdp-linked-list/src/linked_list.cpp
+++ HWCDP/hdp-linked-list/src/linked_list.cpp
@@ -4,11 +4,40 @@
  * You need the implement the methods in this class according to the task description.
  */
 
-OrderedLinkedList::OrderedLinkedList() {}
+OrderedLinkedList::OrderedLinkedList() {
+    // head = new Node(-1,-1);
+}
 
-OrderedLinkedList::~OrderedLinkedList() {}
+OrderedLinkedList::~OrderedLinkedList() {
+    Node* current = head;
+    while (current != nullptr) {
+        Node* next = current->next_node;
+        delete current;
+        current = next;
+    }
+    head = nullptr;
+}
 
-void OrderedLinkedList::add(int key, int value) {}
+void OrderedLinkedList::add(int key, int value) {
+    if (head == nullptr) {
+        head = new Node(key, value);
+        return;
+    }
+    Node* current = head;
+    Node* prev = nullptr;
+    while (current != nullptr && current->key < key) {
+        prev = current;
+        current = current->next_node;
+    }
+    if (prev == nullptr) {
+        head = new Node(key, value);
+        head->next_node = current;
+    } else {
+        prev->next_node = new Node(key, value);
+        prev->next_node->next_node = current;
+    }
+
+}
 
 void OrderedLinkedList::remove(int key) {}
 
